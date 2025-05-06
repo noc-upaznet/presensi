35c63416acb179ec9ec80000319bbb17c063b4ef
rizkaastnoc <astnoc.upaznet@gmail.com>
2025-05-06 15:58:16 +0700

    revisi tampilan pdf

--- app/Http/Controllers/SlipGajiController.php
+++ app/Http/Controllers/SlipGajiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Barryvdh\DomPDF\Facade\Pdf;
+use Illuminate\Http\Request;
 
 class SlipGajiController extends Controller
 {
--- resources/views/livewire/cetak-slip-gaji.blade.php
+++ resources/views/livewire/cetak-slip-gaji.blade.php
@@ -14,69 +14,63 @@
         }
 
         body {
-            font-family: 'Segoe UI', Tahoma, Arial, Helvetica, sans-serif;
+            font-family: Arial, Helvetica, sans-serif;
             margin: 20px auto;
             max-width: 900px;
             color: #000;
             font-size: 12px;
         }
 
-        .header {
-            display: flex;
-            justify-content: space-between;
-            align-items: flex-start;
-        }
-
         .logo {
             height: 30px;
         }
 
-        h2 {
-            margin: 2px 0;
-            font-size: 16px;
-        }
-
-        .header .logo-container {
-            text-align: center;
-        }
-
-        .header .logo-container h2 {
-            margin: 0;
-        }
-
-        .header .logo-container p {
-            margin: 0;
-            font-size: 12px;
-        }
-
-        .header .info {
-            display: flex;
-            justify-content: space-between;
+        .header-slip {
             width: 100%;
+            padding: 10px;
+            font-size: 12px;
+            color: #000;
         }
 
-        .info {
-            margin-top: 10px;
-            display: grid;
-            grid-template-columns: 1fr 1fr;
-            gap: 10px;
-            line-height: 1.4;
+        .header-slip td {
+            vertical-align: top;
         }
 
-        .section {
-            margin-top: 20px;
+        .header-slip .logo {
+            width: 60px;
+            height: auto;
         }
 
-        .grid-2 {
-            display: grid;
-            grid-template-columns: 1fr 1fr;
-            gap: 20px;
+        .company-info {
+            font-family: Arial, sans-serif;
+            padding-left: 10px;
+        }
+
+        .company-info h2 {
+            color: darkblue;
+            font-size: 16px;
+            margin: 0;
+            padding: 0;
+        }
+
+        .company-info p {
+            margin: 2px 0;
+        }
+
+        .slip-info {
+            text-align: right;
+            font-weight: bold;
+        }
+
+        .slip-number {
+            margin-top: 5px;
+            font-weight: normal;
         }
 
         table {
             width: 100%;
             border-collapse: collapse;
-            margin-top: 5px;
+            margin-top: 10px;
         }
 
         th {
@@ -91,6 +85,18 @@
             border-bottom: 1px solid #ddd;
         }
 
+        th.green,
+        th.blue {
+            background-color: #4CAF50;
+            /* green */
+            color: white;
+        }
+
+        th.blue {
+            background-color: #2196F3;
+            /* blue */
+        }
+
         .summary-box {
             border: 2px solid #000;
             padding: 12px;
@@ -99,151 +105,149 @@
             text-align: center;
         }
 
-        .summary-box h2 {
-            margin: 0;
-            font-size: 18px;
-        }
-
         .footer {
-            display: flex;
-            justify-content: space-between;
             font-size: 10px;
             margin-top: 20px;
             color: #666;
         }
 
-        .section-title {
-            font-weight: bold;
-            margin-bottom: 6px;
-            margin-top: 10px;
-        }
-
-        p {
-            margin: 2px 0;
-        }
-
-        /* Menyelaraskan "Benefits" dan "Attendance Summary" */
-        .section.grid-2 {
-            display: flex;
-            justify-content: space-between;
-            gap: 40px;
+        .footer div {
+            display: inline-block;
+            width: 50%;
+            text-align: center;
         }
     </style>
 </head>
 
 <body>
 
-    <div class="header">
-        <div class="logo-container">
-            <div style="display: flex; align-items: center; text-align: left; line-height: 1.5;">
-                <img src="./assets/img/upaznet-logonew.png" alt="Logo" style="height: 40px; margin-right: 15px;">
-                <h2 style="margin: 0; font-size: 14px;">PT Dimensi Jaringan Bersinar</h2>
-            </div>
-
-            <div style="flex: 2; text-align: right;">
-                <h3 style="margin: 0; font-size: 14px; color: navy; font-weight: bold;">SLIP GAJI</h3>
-            </div>
-        </div>
-    </div>
-
-    <table style="width: 100%; border-collapse: collapse; line-height: 1.5; text-align: justify;">
+    <table class="header-slip">
         <tr>
-            <td style="border: none;"><strong>Payroll cut off:</strong> 1 Mar – 31 Mar 2025</td>
-            <td style="border: none;"><strong>Jabatan:</strong> Admin HR</td>
-        </tr>
-        <tr>
-            <td style="border: none;"><strong>ID / Nama:</strong> UZ01 / Nadia Safira</td>
-            <td style="border: none;"><strong>PTKP:</strong> TK/0</td>
-        </tr>
-        <tr>
-            <td style="border: none;"><strong>Departemen:</strong> HR & GA</td>
-            <td style="border: none;"><strong>NPWP:</strong> -</td>
-        </tr>
-        <tr>
-            <td style="border: none;"><strong>Grade / Level:</strong> Staf</td>
-            <td style="border: none;"><strong>Tanggal Masuk:</strong> 22 Jan 2022</td>
+            <td style="width: 70px;">
+                <img src="./assets/img/logo.png" alt="Logo" class="logo">
+            </td>
+            <td style="width: 65%;">
+                <div class="company-info">
+                    <h2>PT DIMENSI JARINGAN BERSINAR</h2>
+                    <p>Tulungagung, Jawa Timur, Indonesia</p>
+                    <p><a href="https://www.upaznet.com" style="color: blue;">www.upaznet.com</a></p>
+                    <p class="slip-number">Slip Number&nbsp;&nbsp;&nbsp;: DJB/HR/SG/25/IV/013</p>
+                </div>
+            </td>
+            <td class="slip-info">
+                SLIP GAJI<br>
+                <span style="font-weight: normal;">April 2025</span>
+            </td>
         </tr>
     </table>
 
-    <div class="section grid-2">
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th colspan="2">Earnings</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Gaji Pokok</td>
-                        <td>Rp2.000.000</td>
-                    </tr>
-                    <tr>
-                        <td>Tunjangan Jabatan</td>
-                        <td>Rp500.000</td>
-                    </tr>
-                    <tr>
-                        <td>Uang Makan</td>
-                        <td>Rp260.000</td>
-                    </tr>
-                    <tr>
-                        <td>Lembur</td>
-                        <td>-</td>
-                    </tr>
-                    <tr>
-                        <td>Bonus/PPh 21 DTP</td>
-                        <td>-</td>
-                    </tr>
-                    <tr>
-                        <th>Total</th>
-                        <th>Rp2.760.000</th>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th colspan="2">Deductions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>BPJS Kesehatan</td>
-                        <td>Rp20.000</td>
-                    </tr>
-                    <tr>
-                        <td>BPJS TK JHT</td>
-                        <td>Rp40.000</td>
-                    </tr>
-                    <tr>
-                        <td>BPJS TK JP</td>
-                        <td>Rp30.000</td>
-                    </tr>
-                    <tr>
-                        <td>PPh 21</td>
-                        <td>Rp15.000</td>
-                    </tr>
-                    <tr>
-                        <td>Potongan Absen</td>
-                        <td>-</td>
-                    </tr>
-                    <tr>
-                        <th>Total</th>
-                        <th>Rp105.000</th>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
+    <table style="width: 100%; font-size: 12px; margin-top: 5px; border-collapse: collapse;">
+        <tr>
+            <td style="width: 50%; padding: 2px 2px 2px 20px; border: none;"><strong>Payroll cut off:</strong> 1 Mar –
+                31 Mar 2025</td>
+            <td style="width: 50%; padding: 2px; border: none;"><strong>Jabatan:</strong> Admin HR</td>
+        </tr>
+        <tr>
+            <td style="padding: 2px 2px 2px 20px; border: none;"><strong>ID / Nama:</strong> UZ01 / Nadia Safira</td>
+            <td style="padding: 2px; border: none;"><strong>PTKP:</strong> TK/0</td>
+        </tr>
+        <tr>
+            <td style="padding: 2px 2px 2px 20px; border: none;"><strong>Departemen:</strong> HR & GA</td>
+            <td style="padding: 2px; border: none;"><strong>NPWP:</strong> -</td>
+        </tr>
+        <tr>
+            <td style="padding: 2px 2px 2px 20px; border: none;"><strong>Grade / Level:</strong> Staf</td>
+            <td style="padding: 2px; border: none;"><strong>Tanggal Masuk:</strong> 22 Jan 2022</td>
+        </tr>
+    </table>
 
-    <div class="summary-box">
-        <p>Total Take Home Pay</p>
-        <h2>Rp2.655.000</h2>
-    </div>
 
-    <table style="width: 100%; border-collapse: collapse; font-family: Arial, sans-serif; font-size: 14px;">
+    <table style="width: calc(100% - 40px); margin: 10px 20px 0 20px; border-collapse: collapse;">
+        <thead>
+            <tr>
+                <th class="green" colspan="2" style="text-align: left;">Earnings</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Gaji Pokok</td>
+                <td>Rp2.000.000</td>
+            </tr>
+            <tr>
+                <td>Tunjangan Jabatan</td>
+                <td>Rp500.000</td>
+            </tr>
+            <tr>
+                <td>Uang Makan</td>
+                <td>Rp260.000</td>
+            </tr>
+            <tr>
+                <td>Lembur</td>
+                <td>-</td>
+            </tr>
+            <tr>
+                <td>Bonus/PPh 21 DTP</td>
+                <td>-</td>
+            </tr>
+            <tr>
+                <th>Total</th>
+                <th>Rp2.760.000</th>
+            </tr>
+        </tbody>
+    </table>
+
+
+    <table style="width: calc(100% - 40px); margin: 10px 20px 0 20px; border-collapse: collapse;">
+        <thead>
+            <tr>
+                <th colspan="2" style="text-align: left; background-color: #dc3545; color: white;">Deductions</th>
+
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>BPJS Kesehatan</td>
+                <td>Rp20.000</td>
+            </tr>
+            <tr>
+                <td>BPJS TK JHT</td>
+                <td>Rp40.000</td>
+            </tr>
+            <tr>
+                <td>BPJS TK JP</td>
+                <td>Rp30.000</td>
+            </tr>
+            <tr>
+                <td>PPh 21</td>
+                <td>Rp15.000</td>
+            </tr>
+            <tr>
+                <td>Potongan Absen</td>
+                <td>-</td>
+            </tr>
+            <tr>
+                <th>Total</th>
+                <th>Rp105.000</th>
+            </tr>
+        </tbody>
+    </table>
+
+
+    <table style="width: calc(100% - 40px); margin: 10px 20px 0 20px; border-collapse: collapse; font-size: 14px;">
+        <tr>
+            <td colspan="2" style="padding: 0;">
+                <div
+                    style="background-color: #007bff; color: white; padding: 8px 16px; font-weight: bold; font-size: 16px; display: flex; justify-content: space-between;">
+                    <span>Total Take Home Pay</span>
+                    <span>Rp2.655.000</span>
+                </div>
+            </td>
+        </tr>
+    </table>
+
+
+    <table
+        style="width: calc(100% - 40px); margin: 20px 20px 0 20px; border-collapse: collapse; font-family: Arial, sans-serif; font-size: 12px;">
         <tr>
             <td style="vertical-align: top; width: 50%; padding: 0 8px;">
                 <div style="font-weight: bold; border-bottom: 1px solid #ccc; padding-bottom: 4px; margin-bottom: 6px;">
@@ -305,10 +309,10 @@
     </table>
 
 
-    <div class="footer" style="display: flex; justify-content: space-between;">
-        <div>Generated by Zemangat</div>
-        <div>Generated on: 30/04/2025 10:23</div>
+    <div class="footer" style="width: calc(100% - 40px); margin: 30px 20px 0 20px; font-size: 12px;">
+        <div style="text-align: left;">Generated on: 30/04/2025 10:23</div>
     </div>
+
 
 </body>
 
