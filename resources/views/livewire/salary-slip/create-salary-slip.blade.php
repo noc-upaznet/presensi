22761e714e2604dc30a231a8304118e084ee8d37
rizkaastnoc <astnoc.upaznet@gmail.com>
2025-05-31 11:23:07 +0700

    revisi create slip gaji

--- resources/views/livewire/salary-slip/create-salary-slip.blade.php
+++ resources/views/livewire/salary-slip/create-salary-slip.blade.php
@@ -20,65 +20,28 @@
         <div class="card mb-3">
             <div class="card-body">
                 <div class="row">
-                    <div class="col-md-6">
-                        <label>Slip Number</label>
-                        <input type="text" class="form-control" placeholder="Contoh: DJB/HR/SG/25/IV/013">
-                    </div>
-                    <div class="col-md-6">
-                        <label>Periode</label>
-                        <input type="text" class="form-control" placeholder="April 2025">
-                    </div>
-                </div>
-
-                <div class="row mt-3">
-                    <div class="col-md-6">
-                        <label>Payroll Cut Off</label>
-                        <input type="text" class="form-control" placeholder="Contoh: 1 Mar - 31 Mar 2025">
-                    </div>
-                    <div class="col-md-6">
-                        <label>Tanggal Masuk</label>
-                        <input type="date" class="form-control">
-                    </div>
-                </div>
-
-                <div class="row mt-3">
                     <div class="col-md-4">
-                        <label>ID / Nama</label>
-                        <input type="text" class="form-control" placeholder="UZ01 / Nadia Safira">
+                        <label>NPK/NIP</label>
+                        <input type="text" class="form-control" placeholder="Contoh: 241120901">
                     </div>
                     <div class="col-md-4">
-                        <label>Jabatan</label>
-                        <input type="text" class="form-control" placeholder="Admin HR">
+                        <label>Nama</label>
+                        <input type="text" class="form-control" placeholder="Contoh: Nadia Safira">
                     </div>
                     <div class="col-md-4">
                         <label>Departemen</label>
-                        <input type="text" class="form-control" placeholder="HR & GA">
-                    </div>
-                </div>
-
-                <div class="row mt-3">
-                    <div class="col-md-4">
-                        <label>PTKP</label>
-                        <input type="text" class="form-control" placeholder="TK/0">
-                    </div>
-                    <div class="col-md-4">
-                        <label>NPWP</label>
-                        <input type="text" class="form-control" placeholder="-">
-                    </div>
-                    <div class="col-md-4">
-                        <label>Grade / Level</label>
-                        <input type="text" class="form-control" placeholder="Staf">
+                        <input type="text" class="form-control" placeholder="Contoh: HR & GA">
                     </div>
                 </div>
             </div>
         </div>
 
         <div class="card mb-3">
-            <div class="card-header bg-success text-white">Earnings</div>
+            <div class="card-header bg-success text-white">PENDAPATAN</div>
             <div class="card-body">
                 <div class="row">
                     <div class="col-md-4">
-                        <label>Gaji Pokok</label>
+                        <label>Upah Pokok</label>
                         <input type="text" class="form-control" placeholder="Rp2.000.000">
                     </div>
                     <div class="col-md-4">
@@ -86,17 +49,35 @@
                         <input type="text" class="form-control" placeholder="Rp500.000">
                     </div>
                     <div class="col-md-4">
-                        <label>Uang Makan</label>
-                        <input type="text" class="form-control" placeholder="Rp260.000">
+                        <label>Tunjangan Kehadiran</label>
+                        <input type="text" class="form-control" placeholder="Rp300.000">
                     </div>
                 </div>
                 <div class="row mt-3">
-                    <div class="col-md-6">
-                        <label>Lembur</label>
+                    <div class="col-md-4">
+                        <label>Upah Lembur</label>
                         <input type="text" class="form-control" placeholder="-">
                     </div>
-                    <div class="col-md-6">
-                        <label>Bonus / PPh 21 DTP</label>
+                    <div class="col-md-4">
+                        <label>Uang Makan</label>
+                        <input type="text" class="form-control" placeholder="Rp1.000.000">
+                    </div>
+                    <div class="col-md-4">
+                        <label>Uang Transport</label>
+                        <input type="text" class="form-control" placeholder="-">
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-md-4">
+                        <label>Bonus</label>
+                        <input type="text" class="form-control" placeholder="-">
+                    </div>
+                    <div class="col-md-4">
+                        <label>Tunjangan Kebudayaan</label>
+                        <input type="text" class="form-control" placeholder="Rp1.000.000">
+                    </div>
+                    <div class="col-md-4">
+                        <label>Fee Belajar</label>
                         <input type="text" class="form-control" placeholder="-">
                     </div>
                 </div>
@@ -104,64 +85,44 @@
         </div>
 
         <div class="card mb-3">
-            <div class="card-header bg-danger text-white">Deductions</div>
+            <div class="card-header bg-danger text-white">POTONGAN</div>
             <div class="card-body">
                 <div class="row">
                     <div class="col-md-4">
-                        <label>BPJS Kesehatan</label>
+                        <label>Potongan Terlambat</label>
                         <input type="text" class="form-control" placeholder="Rp20.000">
                     </div>
                     <div class="col-md-4">
-                        <label>BPJS TK JHT</label>
+                        <label>Potongan Ijin</label>
                         <input type="text" class="form-control" placeholder="Rp40.000">
                     </div>
                     <div class="col-md-4">
-                        <label>BPJS TK JP</label>
-                        <input type="text" class="form-control" placeholder="Rp30.000">
-                    </div>
-                </div>
-                <div class="row mt-3">
-                    <div class="col-md-6">
-                        <label>PPh 21</label>
-                        <input type="text" class="form-control" placeholder="Rp15.000">
-                    </div>
-                    <div class="col-md-6">
-                        <label>Potongan Absen</label>
+                        <label>Kas bon</label>
                         <input type="text" class="form-control" placeholder="-">
                     </div>
                 </div>
-            </div>
-        </div>
-
-        <div class="card mb-3">
-            <div class="card-header bg-light fw-bold">Benefits</div>
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-md-4">
-                        <label>Total PPh 21 DTP Received</label>
-                        <input type="text" class="form-control" placeholder="Rp0">
-                    </div>
+                <div class="row mt-3">
                     <div class="col-md-4">
                         <label>JKK</label>
-                        <input type="text" class="form-control" placeholder="Rp50.000">
+                        <input type="text" class="form-control" placeholder="-">
                     </div>
                     <div class="col-md-4">
                         <label>JKM</label>
-                        <input type="text" class="form-control" placeholder="Rp50.000">
+                        <input type="text" class="form-control" placeholder="-">
+                    </div>
+                    <div class="col-md-4">
+                        <label>JHT Karyawan</label>
+                        <input type="text" class="form-control" placeholder="-">
                     </div>
                 </div>
                 <div class="row mt-3">
                     <div class="col-md-4">
-                        <label>JHT Company</label>
-                        <input type="text" class="form-control" placeholder="Rp100.000">
+                        <label>Kesehatan</label>
+                        <input type="text" class="form-control" placeholder="-">
                     </div>
                     <div class="col-md-4">
-                        <label>JP Company</label>
-                        <input type="text" class="form-control" placeholder="Rp80.000">
-                    </div>
-                    <div class="col-md-4">
-                        <label>BPJS Kesehatan Company</label>
-                        <input type="text" class="form-control" placeholder="Rp120.000">
+                        <label>Voucher</label>
+                        <input type="text" class="form-control" placeholder="-">
                     </div>
                 </div>
             </div>
@@ -173,7 +134,7 @@
                 <div class="row">
                     <div class="col-md-4">
                         <label>Working Days</label>
-                        <input type="number" class="form-control" placeholder="23">
+                        <input type="number" class="form-control" placeholder="26">
                     </div>
                     <div class="col-md-4">
                         <label>Days Off</label>
@@ -181,17 +142,7 @@
                     </div>
                     <div class="col-md-4">
                         <label>National Holiday</label>
-                        <input type="number" class="form-control" placeholder="0">
-                    </div>
-                </div>
-                <div class="row mt-3">
-                    <div class="col-md-4">
-                        <label>Company Holiday</label>
-                        <input type="number" class="form-control" placeholder="0">
-                    </div>
-                    <div class="col-md-4">
-                        <label>Attendance Code</label>
-                        <input type="text" class="form-control" placeholder="H:1 A:0 SL:2">
+                        <input type="number" class="form-control" placeholder="2">
                     </div>
                 </div>
             </div>
@@ -199,16 +150,7 @@
 
         <div class="text-end">
             <button class="btn btn-primary">Simpan Slip Gaji</button>
+            <button class="btn btn-secondary">Kembali</button>
         </div>
     </div>
 </div>
-
-<script>
-    document.addEventListener("DOMContentLoaded", function () {
-        document.querySelectorAll("input[type='text']").forEach(function (input) {
-            input.addEventListener("focus", function () {
-                this.select();
-            });
-        });
-    });
-</script>
