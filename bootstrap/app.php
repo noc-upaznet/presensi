34bbf3f2a3cad499caeb2c4c9ce6131a2080e24e
azzsyf <122683711+azzsyf@users.noreply.github.com>
2025-10-10 08:54:00 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,7 +20,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        $exceptions->renderable(function (TokenMismatchException $e, $request) {
-            return response()->view('errors.419', [], 419);
-        });
+        // $exceptions->renderable(function (TokenMismatchException $e, $request) {
+        //     return response()->view('errors.419', [], 419);
+        // });
     })->create();
