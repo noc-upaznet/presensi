48a9b64934cc7fa74839dbda7d1ebd72a982c11d
astnoc2 <astnoc2.upaznet@gmail.com>
2026-03-31 09:09:06 +0700

    Update EmployeeAbsent.php

--- app/Livewire/EmployeeAbsent.php
+++ app/Livewire/EmployeeAbsent.php
@@ -5,6 +5,7 @@ namespace App\Livewire;
 use App\Models\M_DataKaryawan;
 use App\Models\M_Divisi;
 use App\Models\M_Pengajuan;
+use App\Models\M_Presensi;
 use Livewire\Component;
 use Livewire\WithoutUrlPagination;
 use Livewire\WithPagination;
@@ -29,23 +30,31 @@ class EmployeeAbsent extends Component
         $query = M_Pengajuan::with(['getKaryawan', 'getShift']);
         $entitas = session('selected_entitas', 'UHO');
 
-        $karyawanIdList = M_DataKaryawan::where('entitas', $entitas)->pluck('id');
+        // 🔹 filter karyawan
+        $karyawanQuery = M_DataKaryawan::where('entitas', $entitas);
 
         if ($this->filterDivisi) {
-            $karyawanIdList = M_DataKaryawan::where('entitas', $entitas)
-                ->where('divisi', $this->filterDivisi)
-                ->pluck('id');
+            $karyawanQuery->where('divisi', $this->filterDivisi);
         }
 
         if ($this->search) {
-            $karyawanIdList = M_DataKaryawan::where('entitas', $entitas)
-                ->where('nama_karyawan', 'like', '%' . $this->search . '%')
-                ->pluck('id');
+            $karyawanQuery->where('nama_karyawan', 'like', '%' . $this->search . '%');
         }
 
+        $karyawanIdList = $karyawanQuery->pluck('id');
+
+        // ambil yang sudah presensi hari ini
+        $presensiHariIni = M_Presensi::whereDate('tanggal', now())
+            ->pluck('user_id');
+
+        // query utama
         $datas = $query->whereIn('karyawan_id', $karyawanIdList)
             ->where('status', 1)
             ->whereDate('tanggal', now())
+
+            // tidak ada di presensi
+            ->whereNotIn('karyawan_id', $presensiHariIni)
+
             ->orderBy('tanggal', 'desc')
             ->paginate($this->perPage);
 
