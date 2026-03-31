442cb62b87bef984c17f9d1ea98f8652fce4bd7b
astnoc2 <astnoc2.upaznet@gmail.com>
2026-03-31 14:10:02 +0700

    Update EmployeeAbsent.php

--- app/Livewire/EmployeeAbsent.php
+++ app/Livewire/EmployeeAbsent.php
@@ -4,15 +4,13 @@ namespace App\Livewire;
 
 use App\Models\M_DataKaryawan;
 use App\Models\M_Divisi;
-use App\Models\M_Pengajuan;
-use App\Models\M_Presensi;
 use Livewire\Component;
 use Livewire\WithoutUrlPagination;
 use Livewire\WithPagination;
 
 class EmployeeAbsent extends Component
 {
-    public $perPage = 10;
+    public $perPage = 25;
     use WithPagination, WithoutUrlPagination;
     protected $paginationTheme = 'bootstrap';
 
