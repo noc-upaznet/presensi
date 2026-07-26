689c0e0ae7c92363a0dbd5c700ecd37f33d19ba8
astnoc2 <astnoc2.upaznet@gmail.com>
2026-07-26 11:05:13 +0700

    Update EditPayroll.php

--- app/Livewire/EditPayroll.php
+++ app/Livewire/EditPayroll.php
@@ -127,6 +127,10 @@ class EditPayroll extends Component
 
     private function getEffectiveDay()
     {
+        if (strtolower(trim($this->level)) === 'spv') {
+            return 26;
+        }
+
         return in_array(
             strtolower(trim($this->divisi)),
             ['teknisi', 'pelayanan']
