eb65caeff4eab6b30ffe67b051ca21ff80d28bb6
astnoc2 <astnoc2.upaznet@gmail.com>
2026-07-27 08:37:53 +0700

    Update create-slip-gaji.blade.php

--- resources/views/livewire/create-slip-gaji.blade.php
+++ resources/views/livewire/create-slip-gaji.blade.php
@@ -125,6 +125,14 @@
                                     </div>
                                 </div>
                             </div>
+                            <div class="mb-3">
+                                <label for="churn" class="form-label fw-semibold">Churn</label>
+                                <div class="input-group mb-2">
+                                    <span class="input-group-text">Rp</span>
+                                    <input type="text" id="churn" class="form-control"
+                                        wire:model.lazy="churn">
+                                </div>
+                            </div>
                         @endif
                         {{-- @if ($this->isSalesPositionSpvUGR())
                             <div class="row">
