a304f5cf91a95c9ae19f3095add1fa607dcd7759
astnoc2 <astnoc2.upaznet@gmail.com>
2026-09-26 08:15:29 +0700

    Update JadwalShift.php

--- app/Livewire/Karyawan/JadwalShift.php
+++ app/Livewire/Karyawan/JadwalShift.php
@@ -88,7 +88,7 @@ class JadwalShift extends Component
 
             $query = M_DataKaryawan::whereIn('divisi', [$divisi, 'General Operation'])
                 ->where('status_karyawan', '!=', 'NONAKTIF')
-                ->whereIn('jabatan', [$jabatan, 'General Operation']);
+                ->whereIn('jabatan', [$jabatan, 'GO']);
 
             if ($entitas === 'UNB') {
                 $query->whereIn('entitas', ['UNB', 'UHO']);
@@ -355,7 +355,7 @@ class JadwalShift extends Component
 
             $query->whereHas('getKaryawan', function ($q) use ($divisi, $entitasFilter) {
                 $q->whereIn('divisi', [$divisi, 'General Operation'])
-                    ->whereIn('jabatan', ['Sales Marketing', 'General Operation'])
+                    ->whereIn('jabatan', ['Sales Marketing', 'GO'])
                     ->where('status_karyawan', '!=', 'NONAKTIF')
                     ->whereIn('entitas', $entitasFilter);
             });
